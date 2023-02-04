4f0b783b83a3ade40e4da11d3fc0f95a91249cc4
Panyut Sriwirote <mildpanyut@gmail.com>
2023-02-04 23:13:55 +0700

    add mechanism to deal with missing values

--- DecisionTree/ID3.py
+++ DecisionTree/ID3.py
@@ -4,23 +4,23 @@ from math import log2
 
 class Node:
 
-    def __init__(self, classifier: Union[str, tuple[str, dict[str, "Node"]]]):
+    def __init__(self, classifier: Union[str, tuple[str, dict[str, "Node"], str]]):
         self.classifier = classifier
 
     def classify(self, instance: dict[str, str]) -> str:
         if isinstance(self.classifier, str):
             return self.classifier
         else:
-            test_attribute, children = self.classifier
-            return children[instance[test_attribute]].classify(instance)
+            test_attribute, children, default_value = self.classifier
+            return children.get(instance.get(test_attribute, default_value), children["__other__"]).classify(instance)
 
     def stringify(self, indent_level: int):
         if isinstance(self.classifier, str):
             return self.classifier
         else:
-            test_attribute, children = self.classifier
+            test_attribute, children, default_value = self.classifier
             indentation = '\t' * indent_level
-            return ('\n' if indent_level != 0 else '') + indentation + f"\n{indentation}EL".join(f"IF {test_attribute} = {value} THEN {children[value].stringify(indent_level + 1)}" for value in children)
+            return ('\n' if indent_level != 0 else '') + indentation + f"\n{indentation}EL".join(f"IF {test_attribute} = {value}{' (default)' if value == default_value else ''} THEN {children[value].stringify(indent_level + 1)}" for value in children)
 
 class DecisionTree:
 
@@ -46,33 +46,36 @@ class DecisionTree:
         elif len(attrs) == 0:
             return Node(target_count.most_common(1)[0][0])
         else:
-            optimal_classifier = self.get_optimal_classifier(examples, target, attrs)
+            optimal_classifier, default_value = self.get_optimal_classifier(examples, target, attrs)
             children: dict[str, Node] = {}
-            possible_values = {example[optimal_classifier] for example in examples}
+            possible_values = {example.get(optimal_classifier, default_value) for example in examples}
             for value in possible_values:
-                sub_group = [example for example in examples if example[optimal_classifier] == value]
-                if len(sub_group) == 0:
-                    children[value] =  Node(target_count.most_common(1)[0][0])
-                else:
-                    children[value] = self.ID3(
-                        sub_group,
-                        target,
-                        attrs - {optimal_classifier}
-                    )
-            return Node((optimal_classifier, children))
+                sub_group = [example for example in examples if example.get(optimal_classifier, default_value) == value]
+                children[value] = self.ID3(
+                    sub_group,
+                    target,
+                    attrs - {optimal_classifier}
+                )
+            children["__other__"] = Node(target_count.most_common(1)[0][0])
+            return Node((optimal_classifier, children, default_value))
 
     def get_optimal_classifier(self, examples: list[dict[str, str]], target: str, attrs: set[str]):
-        return max(attrs, key=lambda attr: self.compute_information_gain(examples, attr, target))
+        information_gain = {
+            attr: self.compute_information_gain(examples, attr, target) for attr in attrs
+        }
+        optimal_classifier = max(information_gain, key=lambda attr: information_gain[attr][0])
+        return optimal_classifier, information_gain[optimal_classifier][1]
 
     def compute_information_gain(self, examples: list[dict[str, str]], attr: str, target: str):
         old_entropy = self.compute_entropy(examples, target)
-        possible_values = {example[attr] for example in examples}
+        possible_values = Counter(example[attr] for example in examples if attr in example)
+        default_value = possible_values.most_common(1)[0][0]
         new_entropy = 0
         num_example = len(examples)
         for value in possible_values:
-            sub_group = [example for example in examples if example[attr] == value]
+            sub_group = [example for example in examples if example.get(attr, default_value) == value]
             new_entropy += (len(sub_group) / num_example) * self.compute_entropy(sub_group, target)
-        return old_entropy - new_entropy
+        return old_entropy - new_entropy, default_value
 
     def compute_entropy(self, examples: list[dict[str, str]], target: str):
         target_value_count = Counter(example[target] for example in examples)
